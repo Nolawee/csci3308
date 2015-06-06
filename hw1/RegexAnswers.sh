e2b3e33a985dc7ea1303ac821e97661e6ac0ac87
Dylan Nguyen <dyng9409@colorado.edu>
2015-06-06 13:46:52 -0600

    finished hw

--- hw1/RegexAnswers.sh
+++ hw1/RegexAnswers.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#Dylan Nguyen
+file=$1
+if [ $# -ne 1 ]
+then
+				echo "Usage: RegexAnswers.sh filename"
+				exit 1
+fi
+egrep '[0-9]$' ${file} | wc -l
+egrep '^[^aiueoAIUEO]' ${file} | wc -l
+egrep '^[a-zA-Z]{12}$' ${file} | wc -l
+egrep '^[0-9]{3}-[0-9]{3}-[0-9]{4}$' ${file} | wc -l
+egrep '^303-441-[0-9]{4}' ${file} | wc -l
+egrep '[A-Za-z0-0._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' ${file} | wc -l
+egrep '[A-Za-z0-0._%+-]+@[A-Za-z0-9.-]+\.gov' ${file} | wc -l
