5b65c0721541b43499772ae9f520e20396f96a68
Dylan Nguyen <dyng9409@colorado.edu>
2015-06-08 11:06:46 -0600

    updating files, bug fixes

--- hw1/RegexAnswers.sh
+++ hw1/RegexAnswers.sh
@@ -11,5 +11,5 @@ egrep '^[^aiueoAIUEO]' ${file} | wc -l
 egrep '^[a-zA-Z]{12}$' ${file} | wc -l
 egrep '^[0-9]{3}-[0-9]{3}-[0-9]{4}$' ${file} | wc -l
 egrep '^303-441-[0-9]{4}' ${file} | wc -l
-egrep '[A-Za-z0-0._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' ${file} | wc -l
-egrep '[A-Za-z0-0._%+-]+@[A-Za-z0-9.-]+\.gov' ${file} | wc -l
+egrep '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' ${file} | wc -l
+egrep '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.gov' ${file} | wc -l
