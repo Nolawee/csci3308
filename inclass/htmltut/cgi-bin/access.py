a1eddf4e6aa76907180da46cb80f49c696a7fb64
Dylan Nguyen <dyng9409@colorado.edu>
2015-06-18 19:39:36 -0600

    finished hw4

--- inclass/htmltut/cgi-bin/access.py
+++ inclass/htmltut/cgi-bin/access.py
@@ -14,8 +14,18 @@ db = MySQLdb.connect(host= 'localhost',
 #create cursor object
 cur = db.cursor()
 #use all SQL you want
-cur.execute("SELECT * FROM Dept")
-
+cur.execute("SELECT * FROM store")
+print "<table border=1px dashed black>"
+print "<tr><th>Quantity</th><th>Price</th><th>Cost</th></tr>"
+sum=0
 for row in cur.fetchall() :
-    print row[0]
+    #print row[0]
+    #print row[1]
+    qty = row[2]
+    price = row[3]
+    sum=sum+qty*price
+    print "<tr><td>", qty, "</td><td>", price, "</td><td>", (qty*price), "</td></tr>"
+print"</table>"
+print "Total inventory cost: $", sum
+
 print "DONE"
